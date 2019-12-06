f9ac8a7567e887900dfe7c9ced3ba7c536011e87
TN <thu.thao.nguyen.ngoc@uni-weimar.de>
2019-12-06 10:12:51 +0100

    Switched back to using double since it's much shorter

--- LINES/Hans-Thao-Anton/lines.cpp
+++ LINES/Hans-Thao-Anton/lines.cpp
@@ -1,48 +1,10 @@
 // https://www.spoj.com/problems/LINES/
 
 #include <iostream>
-#include <set>
-#include <map>
+#include <unordered_set>
+#define max 9999
 
 using namespace std;
-
-struct Fraction {
-	int num;
-	int den;
-
-	bool operator < (const Fraction& f) const
-	{
-		return den < f.den || (den == f.den && num < f.num);
-	}
-};
-
-Fraction reduce(Fraction f)
-{
-	int a = f.num;
-	int b = f.den;
-
-	while (b != 0)
-	{
-		int temp = b;
-		b = a % b;
-		a = temp;
-	}
-
-	int gcd = a;
-	f.num = f.num / gcd;
-	f.den = f.den / gcd;
-	return f;
-}
-
-Fraction get_m(int x1, int y1, int x2, int y2)
-{
-	int dy = y1 - y2;
-	int dx = x1 - x2;
-	Fraction m;
-	m.num = dy;
-	m.den = dx;
-	return reduce(m);
-}
 
 int main()
 {
@@ -54,9 +16,8 @@ int main()
 	for (;;)
 	{
 		cin >> n;
-		pair<int, int> points[n];
-		Fraction m;
-		set<Fraction> slopes;
+		pair<double, double> points[n];
+		unordered_set<double> slopes;
 
 		if (n == 0)
 		{
@@ -66,13 +27,21 @@ int main()
 		for (int i=0; i<n; i++)
 		{
 			cin >> points[i].first >> points[i].second;
-		}
 
-		for (int i=0; i<n; i++)
-		{
-			for (int j=i+1; j<n; j++)
+			for (int j=i-1; j>=0; j--)
 			{
-				m = get_m(points[i].first, points[i].second, points[j].first, points[j].second);
+				double m;
+
+				if (points[i].first == points[j].first)
+				{
+					m = max;
+				}
+
+				else
+				{
+					m = (points[i].second - points[j].second) / (points[i].first - points[j].first);
+				}
+
 				slopes.insert(m);
 			}
 		}
