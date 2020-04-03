cbcd8738c1e3830a487d9a6f223daf5c5c2e3cc2
Prem Kumar Tiwari <31136562+ptiwari2407@users.noreply.github.com>
2020-04-03 10:44:00 +0200

    Add files via upload

---
+++ Prem/BRKDB/BRKDB.py
@@ -0,0 +1,9 @@
+# https://www.spoj.com/problems/BRKDB/
+# python 2 use raw_input()
+X = raw_input()
+monsalve = "Monsalve 200410061010"
+silva = "Silva 200120039010"
+Pineda ="Pineda 199810024010"
+print(X + " " + silva)
+print(X + " " + Pineda)
+print(X + " " + monsalve)
