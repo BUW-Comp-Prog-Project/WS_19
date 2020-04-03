d664b514db6f70f52e5d2840b54671c509b3524b
Prem Kumar Tiwari <31136562+ptiwari2407@users.noreply.github.com>
2020-04-03 09:52:21 +0200

    Add files via upload

---
+++ Prem/CHITEST1/CHITEST1.py
@@ -0,0 +1,11 @@
+# https://www.spoj.com/problems/CHITEST1/
+t = int(input())
+while(t>0):
+    # temp = input()
+    # s = [int(num) for num in temp.split()]
+    # print(s)
+    x, y = input().split()
+    x = float(x)
+    y = float(y)
+    print(x+y)
+    t=t-1
